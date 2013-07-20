a129c984383db1696f4a86b9e9c9e01020f474ff
Bret Comnes <bcomnes@gmail.com>
2013-07-20 01:45:36 -0700

    Reworking the javascript drastically

--- assets/js/webmention.js
+++ assets/js/webmention.js
@@ -1,34 +1,53 @@
-var mentionElements
-var webmentionGet = function(type) {
-	mentionElements = document.getElementsByClassName('webmentions ' + type);
-	for (var i=0; i < mentionElements.length; i++) {
-		var mentionTarget = encodeURIComponent(mentionElements[i].id);
-	    var mentionData = document.createElement('SCRIPT');
-	    var jasonpName = "webmentionProcess";
-	    var token = '';
-	    //var mentionDataUrl = "http://pingback.me/api/links?target="+mentionTarget+"&jsonp=webmentionProcess&access_token="+token;
-	    var mentionDataUrl = "http://pingback.me/api/links?target="+mentionTarget+"&jsonp=webmentionProcess"
-	    mentionData.src = mentionDataUrl;
-	    document.body.appendChild(mentionData)
-	}
+// Global webmention.js object
+var webMention = {};
+console.log('Webmention.js initialized');
+// webMention.get function for finding objects to interact with.
+webMention.get = function(type) {
+	console.log('get function just fired');
+	// Get a list of all elements with the class of webmention and type
+	webMention.elements = document.getElementsByClassName('webmentions ' + type);
+	console.log('Elements were found');
+	console.log(webMention.elements);
+	// Process each element found
+	webMention.preProcess();
 }
 // window.onload = pingback('mode', 'webmentions');
 
-var mentions;
-var webmentionProcess = function ( data ) {
-	console.log(mentions);
+webMention.preProcess = function() {
+	console.log('Preprocess was triggered');
+	for (var i=0; i < webMention.elements.length; i++) {
+		// For a given element, retrieve the URL from its ID and store it
+		webMention.elements[i].slug = webMention.elements[i].id;
+		console.log('Slugs were ammended');
+		console.log(webMention.elements[i].slug);
+		// For a given element, create the uniqe 
+		webMention.elements[i].postProcess = function(data) {
+			console.log('We are attempting to create our object functions');
+			webMention.elements[i].parent = document.getElementById(webMention.elements[i].slug);
+			webMention.elements[i].placeHolder = webMention.elements[i].parent.firstElementChild;
+			webMention.elements[i].parent.removeChild(webMention.elements[i].placeHolder);
+			var linkNode = document.createElement('LI');
+			var testData = document.createTextNode('Huston we have liftoff');
+			linkNode.appendChild(testData);
+			webMention.elements[i].parent.appendChild(linkNode);
+		}
+		console.log('Scripts are about to be inserted');
+		webMention.script(i);
+	}
 }
 
-webmentionGet('links');
-//var parseJson = function( data ){
-//	
-//}
-//
-//webmentionFind('mode', 'webmentions');
-//
-//var placeHolderNode = mentionElements[i].firstElementChild;
-//	    mentionElements[i].removeChild(placeHolderNode);
-//	    var linkNode = document.createElement('LI');
-//	    var testData = document.createTextNode('Huston we have liftoff');
-//	    linkNode.appendChild(testData);
-//	    mentionElements[i].appendChild(linkNode);
+webMention.script = function (i) {
+	console.log('Scripts are about to be inserted');
+	// Create functon attached to element object 
+    var jsonpName = "webMention.elements[" + i + "].postProcess"
+    // Generate the API request URL 
+    var apiUrl = "http://pingback.me/api/links?target="+ webMention.elements[i].slug + "&jsonp=" + jsonpName
+    // Generate the script element to be inserted
+    var container = document.createElement('SCRIPT');
+    // Set the source of the script container
+    container.src = apiUrl;
+    document.head.appendChild(container)
+    console.log('SCripts have been inserted maybe');
+}
+
+webMention.get('links');
