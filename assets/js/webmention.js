ab77a37d82187de38205b50cdfc46054b16e590a
Bret Comnes <bcomnes@gmail.com>
2013-07-23 23:28:55 -0700

    Started work to sort by class

--- assets/js/webmention.js
+++ assets/js/webmention.js
@@ -65,4 +65,18 @@ webMention.postProcess = function(data) {
 			}
 		}
 
-webMention.get('links');
+webMention.hasClass = function( selector ) {
+	console.log(this);
+var className = " " + selector + " ",
+i = 0,
+l = this.length;
+for ( ; i < l; i++ ) {
+if ( this[i].nodeType === 1 && (" " + this[i].className + " ").replace(rclass, " ").indexOf( className ) >= 0 ) {
+return true;
+}
+}
+
+return false;
+}
+
+webMention.get('count');
