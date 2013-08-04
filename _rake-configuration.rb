69b1971bc3a60a6d2aac7a559268aaf41c0512f4
Bret Comnes <bcomnes@gmail.com>
2013-08-03 19:14:47 -0700

    added rake file

---
+++ _rake-configuration.rb
@@ -0,0 +1,7 @@
+# where the Site is going to live
+$deploy_url     = "http://bret.io"
+# the directory on the server (rsync like syntax)
+$deploy_dir     = "git@github.com:bcomnes/bcomnes.github.io.git"
+# the default post directory
+$post_dir       = "_posts/"
+
